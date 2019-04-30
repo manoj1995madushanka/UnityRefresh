98f8db0fe2812b28e5534b40531b3acfeb692290
manoj1995madushanka <madushankamanoj414@gmail.com>
2019-04-30 11:41:30 +0530

    script and force

---
+++ Assets/Scripts/playerMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello world");
+        //rb.useGravity = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce(0,0,1500 * Time.deltaTime);
+    }
+}
