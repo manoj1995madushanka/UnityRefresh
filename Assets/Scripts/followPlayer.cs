f8adf3c19c38884c73ee342490a99271f6407983
manoj1995madushanka <madushankamanoj414@gmail.com>
2019-04-30 15:17:20 +0530

    camera follow player

---
+++ Assets/Scripts/followPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followPlayer : MonoBehaviour
+{
+    public Vector3 offest;
+    public Transform player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // print player location to the console
+        Debug.Log(player.position);
+        // now set player position to the camera
+        transform.position = player.position +offest; 
+    }
+}
